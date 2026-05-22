21e5aac308025c4187919864ef70d77c98c20f30
reshmakr10 <reshmakrishnan311@gmail.com>
2026-05-22 08:38:48 +0000

    fIxed login issue

--- ui-tests/pages/LoginPage.ts
+++ ui-tests/pages/LoginPage.ts
@@ -16,8 +16,8 @@ export class LoginPage {
   }
 
   async goto() {
-    await this.page.goto('/', { waitUntil: 'networkidle' });
-    await this.page.waitForLoadState('networkidle');
+    await this.page.goto('/', { waitUntil: 'load' });
+    await this.page.waitForLoadState('domcontentloaded');
   }
 
   async expectVisible() {
